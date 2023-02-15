7a9e65d29322f3d47248d8a15aa06c7c77bb4bab
sixtusdev <sixtusushahemba@gmail.com>
2023-02-15 12:13:22 +0100

    Commit on index file and Js file

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,21 @@
 function fetch_user_url() {
   window.location.href = 'about.html'
 }
+
+// Get the navbar element
+const navbar = document.querySelector('#navbar')
+
+// Get the offset position of the navbar
+const navbarOffset = navbar.offsetTop
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', () => {
+  // Check if the user has scrolled past the navbar
+  if (window.pageYOffset >= navbarOffset) {
+    // Add the 'scrolled' class to the navbar
+    navbar.classList.add('sticky')
+  } else {
+    // Remove the 'scrolled' class from the navbar
+    navbar.classList.remove('sticky')
+  }
+})
